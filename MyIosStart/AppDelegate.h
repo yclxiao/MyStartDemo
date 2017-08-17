2d6368d564ec511220c81d5317fd006481cf70bd
OF1089（杨成龙） <yangchenglong@qianmi.com>
2017-08-17 19:39:21 +0800

    Initial Commit

---
+++ MyIosStart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyIosStart
+//
+//  Created by yclxiao on 2017/8/17.
+//  Copyright © 2017年 yclxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
